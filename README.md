b45707e1724a6cbf24a03eae2bb49fe959c81a38
dylanft <dylan@wildwestdigital.io>
2022-03-08 10:33:01 -0800

    added tests for loan initialization

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # coinflow
 Proof of Concept for putting collateral to work for a short term loan.
+
+<!-- 
+Example:
+>> ::set_tx_sender ST1SJ3DTE5DN7X54YDH5D64R3BCB6A2AG2ZQ8YPD5
+>> (contract-call? 'ST1PQHQKV0RJXZFY1DGX8MNSNYVE3VGZJSRTPGZGM.coinflow-usda-xbtc signal-interest u1000000 u145)
+
+
+>> ::set_tx_sender ST2CY5V39NHDPWSXMW9QDT3HC3GD6Q6XX4CFRK9AG
+>> ::get_assets_maps
+
+>> (contract-call? 'ST1PQHQKV0RJXZFY1DGX8MNSNYVE3VGZJSRTPGZGM.coinflow-usda-xbtc lock-up-LP-collateral 'ST1SJ3DTE5DN7X54YDH5D64R3BCB6A2AG2ZQ8YPD5 u288 u1000000)
+
+>> ::set_tx_sender ST1SJ3DTE5DN7X54YDH5D64R3BCB6A2AG2ZQ8YPD5
+>> (contract-call? 'ST1PQHQKV0RJXZFY1DGX8MNSNYVE3VGZJSRTPGZGM.coinflow-usda-xbtc send-collateral-to-LP u40 'ST2CY5V39NHDPWSXMW9QDT3HC3GD6Q6XX4CFRK9AG none) -->
--- tests/coinflow-usda-xbtc_test.ts
+++ tests/coinflow-usda-xbtc_test.ts
@@ -3,15 +3,24 @@ import { Clarinet, Tx, Chain, Account, types } from 'https://deno.land/x/clarine
 import { assertEquals } from 'https://deno.land/std@0.90.0/testing/asserts.ts';
 
 Clarinet.test({
-    name: "Ensure that <...>",
+    name: "Ensure loan initialization works as intended. Basic stepthrough.",
     async fn(chain: Chain, accounts: Map<string, Account>) {
+        let deployer = accounts.get("deployer")!.address;
+        // let coinflow_contract = accounts.get("wallet_1")!.address;
+        let borrower = accounts.get("wallet_1")!.address;
+        let lender = accounts.get("wallet_2")!.address;
+
         let block = chain.mineBlock([
             /* 
              * Add transactions with: 
              * Tx.contractCall(...)
             */
+            Tx.contractCall("ST1PQHQKV0RJXZFY1DGX8MNSNYVE3VGZJSRTPGZGM.coinflow-usda-xbtc","signal-interest", ["u1000000", "u145"], borrower),
+            Tx.contractCall("coinflow-usda-xbtc","lock-up-LP-collateral", [types.principal(borrower), "u288", "u1000000"], lender),
+            // Tx.contractCall("coinflow-usda-xbtc","send-collateral-to-LP", ["u40", types.principal(lender), "none"], borrower)
+
         ]);
-        assertEquals(block.receipts.length, 0);
+        // assertEquals(block.receipts.length, 0);
         assertEquals(block.height, 2);
 
         block = chain.mineBlock([
@@ -19,8 +28,20 @@ Clarinet.test({
              * Add transactions with: 
              * Tx.contractCall(...)
             */
+            // Tx.contractCall("ST1PQHQKV0RJXZFY1DGX8MNSNYVE3VGZJSRTPGZGM.coinflow-usda-xbtc","get-loan-interest-from", [borrower], borrower), 
+            // Tx.contractCall("ST1PQHQKV0RJXZFY1DGX8MNSNYVE3VGZJSRTPGZGM.coinflow-usda-xbtc","lock-up-LP-collateral", [borrower, "u288", "u1000000"], lender.address)
+
         ]);
-        assertEquals(block.receipts.length, 0);
+
+
+        let loanHealth = chain.callReadOnlyFn('coinflow-usda-xbtc', 'get-loan-health', [types.principal(borrower), types.principal(lender)], borrower);
+        console.log(loanHealth);
+        
+
+        let loanBalance = chain.callReadOnlyFn('coinflow-usda-xbtc', 'get-loan-balance', [types.principal(borrower), types.principal(lender)], borrower);
+        console.log(loanBalance);
+        // loanBalance.result.expectUint(1000000)
+        // assertEquals(block.receipts.length, 0);
         assertEquals(block.height, 3);
     },
 });
